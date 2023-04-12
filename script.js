4c933beb45bab6bb4b952e30285895ffd90358bc
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-04-11 20:02:01 -0400

    need to go back and take a look through everything

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-const slots = document.querySelectorAll('.square-slot')
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+}
 
 
-
-
-
-
-
-
-
-
-
-// const game = {
-//   xTurn: true,
-//   xState: [],
-//   oState: [],
-//   winningStates: [
-//       // Rows
-//       ['0', '1', '2'],
-//       ['3', '4', '5'],
-//       ['6', '7', '8'],
-
-//       // Columns
-//       ['0', '3', '6'],
-//       ['1', '4', '7'],
-//       ['2', '5', '8'],
-
-//       // Diagonal
-//       ['0', '4', '8'],
-//       ['2', '4', '6']
-//   ]
-// }
-
-
-// document.addEventListener('click', event => {
-//   const target = event.target
-//   const isCell = target.classList.contains('square-slot')
-//   const isDisabled = target.classList.contains('disabled')
-
-//   if (isCell && !isDisabled) {
-//     const cellValue = target.dataset.value
-
-// game.xTurn === true
-//     ? game.xState.push(cellValue)
-//     : game.oState.push(cellValue)
-
-// target.classList.add('disabled')
-// target.classList.add(game.xTurn ? 'x' : 'o')
-
-// game.xTurn = !game.xTurn
-//       // The player clicked on a cell that is still empty
-//   }
-// })
-
-// if (!document.querySelectorAll('.square-slot:not(.disabled)').length) {
-//   document.querySelector('.game-over').classList.add('visible')
-//   document.querySelector('.game-over-text').textContent = 'Draw!'
-// }
-
-// game.winningStates.forEach(winningState => {
-//   const xWins = winningState.every(state => game.xState.includes(state))
-//   const oWins = winningState.every(state => game.oState.includes(state))
-
-//   if (xWins || oWins) {
-//       document.querySelectorAll('.square-slot').forEach(cell => cell.classList.add('disabled'))
-//       document.querySelector('.game-over').classList.add('visible')
-//       document.querySelector('.game-over-text').textContent = xWins
-//           ? 'X wins!'
-//           : 'O wins!'
-//   }
-// })
-
-// document.querySelector('.restart').addEventListener('click', () => {
-//   document.querySelector('.game-over').classList.remove('visible')
-//   document.querySelectorAll('.square-slot').forEach(cell => {
-//       cell.classList.remove('disabled', 'x', 'o')
-//   })
-
-//   game.xTurn = true
-//   game.xState = []
-//   game.oState = []
-// })
